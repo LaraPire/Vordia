d0d7c06645205603e2e1def1a99d23cf7e9a3ca8
Raymond Baghumian <swordart2050@gmail.com>
2025-02-02 03:32:56 +0330

    Update Admin Index

--- src/Resources/views/admin/index.blade.php
+++ src/Resources/views/admin/index.blade.php
@@ -1,4 +1,12 @@
 @extends('Vordia::layouts.admin')
 @section('content')
-    تست ۱
+    <div class="text-center" style="margin-top: 300px">
+        <img src="https://raw.githubusercontent.com/Rayiumir/Vordia/refs/heads/main/art/Vordia.png" alt="" srcset="">
+        <p> Vordia Mobile Authentication Laravel </p>
+        <div class="mt-3">
+            <img alt="GitHub followers" src="https://img.shields.io/github/followers/rayiumir">
+            <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/rayiumir/vordia">
+            <img alt="GitHub forks" src="https://img.shields.io/github/forks/rayiumir/vordia">
+        </div>
+    </div>
 @endsection
