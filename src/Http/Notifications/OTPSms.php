c0d720f507c7ce7942dcab5c578a07b7d249ecf3
amir-ys <amir.you74@gmail.com>
2025-03-23 20:21:21 +0330

    remove toMail method from OTPsms notification

--- src/Http/Notifications/OTPSms.php
+++ src/Http/Notifications/OTPSms.php
@@ -3,7 +3,6 @@
 namespace Rayiumir\Vordia\Http\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Rayiumir\Vordia\Http\Channels\SmsChannel;
 
@@ -32,29 +31,19 @@ class OTPSms extends Notification
     }
 
     /**
-     * Get the mail representation of the notification.
-     */
-    public function toMail($notifiable): MailMessage
-    {
-        return (new MailMessage)
-            ->line('The introduction to the notification.')
-            ->action('Notification Action', url('/'))
-            ->line('Thank you for using our application!');
-    }
-
-    /**
      * Get the array representation of the notification.
      *
      * @return array<string, mixed>
      */
     public function toArray($notifiable): array
     {
-        return [//
-        ];
+        return [];
     }
 
     public function toSms($notifiable)
     {
-        return ['code' => $this->code];
+        return [
+            'code' => $this->code
+        ];
     }
 }
